41e0c3a455f4a79943d31f95b481884071a4d09f
Alissa Avtonoskina <alissa.avt@gmail.com>
2025-03-27 00:44:12 +0100

    styling and links to maps

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ type Station = {
   address: string;
   postcode: string;
   district: string;
+  lat: number;
+  lng: number;
 };
 
 function App() {
@@ -50,7 +52,9 @@ function App() {
           district: feature.attributes.adresse.substring(
             feature.attributes.adresse.lastIndexOf(' ') + 1,
             feature.attributes.adresse.lastIndexOf(')')
-          )
+          ),
+          lat: feature.geometry.y,
+          lng: feature.geometry.x
         }));
         setStations(parsedStations);
       });
@@ -78,25 +82,39 @@ function App() {
   })
   
   const rows = sortedStations.map(station => 
-  <tr>
-    <td className="address">{station.address}</td> 
-    <td className="postcode">{station.postcode}</td> 
-    <td className="district">{station.district}</td> 
-  </tr>
+    <a href={"https://www.google.com/maps/place/" + station.lat + "," + station.lng} target="_blank" className="">
+      <div className="flex flex-row w-full gap-1 rounded-md p-1 hover:bg-[#1a1a1a]">
+        <span className="address w-[240px] text-left pl-3">{station.address}</span> 
+        <span className="postcode w-[130px]">{station.postcode}</span> 
+        <span className="district w-[200px] text-left pl-3">{station.district}</span> 
+      </div>
+    </a>
+  
   );
 
   return (
     <>
       <h1>Tankstellen in Köln</h1>
-      <input value={search} onChange={handleSearchInput}></input>
-      <table className="station-table">
-        <tr className="table-header">
-          <th className="address" onClick={() => handleOrderChange("asc-address")}>Adresse</th>
-          <th className="postcode" onClick={() => handleOrderChange("asc-postcode")}>Postleitzahl</th> 
-          <th className="district" onClick={() => handleOrderChange("asc-district")}>Stadtteil</th> 
-        </tr>
+
+      <div className="flex flex-row gap-2 items-center my-4 w-full px-2">
+        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 48 48">
+          <path fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="3" d="M32.4,26.2l8.1,8.1c1.7,1.7,1.7,4.5,0,6.2l0,0c-1.7,1.7-4.5,1.7-6.2,0L30,36.2"></path>
+          <path fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="3" d="M8,25c-1.8-4.7-0.8-10.2,3-14c3.8-3.8,9.5-4.8,14.2-2.9"></path>
+          <path fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="3" d="M31.3,13.1c3.4,5.1,2.8,12.1-1.7,16.6c-4.9,4.9-12.6,5.1-17.7,0.8"></path>
+        </svg>
+
+        <input className="pl-2 mr-3 border border-[#646cff] rounded-sm flex-grow" type="search" placeholder='Nach Tankstelle suchen...' value={search} onChange={handleSearchInput}></input>
+
+      </div>
+
+      <div className="flex flex-col mx-auto">
+        <div className="flex flex-row w-full gap-1" >
+          <button className="w-[240px] text-left" onClick={() => handleOrderChange("asc-address")}>Adresse</button>
+          <button className="w-[130px]" onClick={() => handleOrderChange("asc-postcode")}>Postleitzahl</button>
+          <button className="w-[200px] text-left" onClick={() => handleOrderChange("asc-district")}>Stadtteil</button>
+        </div>
         {rows}
-      </table>
+      </div>
     </>
   )
 }
